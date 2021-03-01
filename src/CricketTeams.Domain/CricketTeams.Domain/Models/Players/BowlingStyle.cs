b20b6a0ddb24425e313ee9e3cfc4ddad76a11290
yavaka <yavakasolutions@gmail.com>
2021-03-01 11:09:50 +0000

    Added bowling style model, bowling style data and bowling type enum.

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Players/BowlingStyle.cs
@@ -0,0 +1,46 @@
+﻿namespace CricketTeams.Domain.Models
+{
+    using CricketTeams.Domain.Common;
+    using CricketTeams.Domain.Exceptions;
+
+    using static ModelConstants.BowlingStyle;
+
+    public class BowlingStyle : Entity<int>
+    {
+        internal BowlingStyle(string styleName, BowlingType bowlingType, string description)
+        {
+            this.Validate(styleName, description);
+
+            this.StyleName = styleName;
+            this.Description = description;
+            this.BowlingType = bowlingType;
+        }
+
+        private BowlingStyle(string styleName, string description)
+        {
+            this.StyleName = styleName;
+            this.Description = description;
+
+            this.BowlingType = default!;
+        }
+
+        public string StyleName { get; private set; }
+        public BowlingType BowlingType { get; private set; }
+        public string Description { get; private set; }
+
+        private void Validate(string styleName, string description)
+        {
+            Guard.ForStringLength<InvalidPlayerException>(
+                styleName,
+                MinStyleNameLength,
+                MaxStyleNameLength,
+                nameof(this.StyleName));
+
+            Guard.ForStringLength<InvalidPlayerException>(
+                description,
+                MinDescriptionLength,
+                MaxDescriptionLength,
+                nameof(this.Description));
+        }
+    }
+}
---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Players/BowlingType.cs
@@ -0,0 +1,21 @@
+﻿namespace CricketTeams.Domain.Models
+{
+    using CricketTeams.Domain.Common;
+
+    public class BowlingType : Enumeration
+    {
+        public static readonly BowlingType FastBowling = new BowlingType(1, "Fast bowling");
+        public static readonly BowlingType SpinBowling = new BowlingType(2, "Spin bowling");
+        public static readonly BowlingType LegSpinBowling = new BowlingType(3, "Leg spin bowling");
+
+        private BowlingType(int value)
+            : this(value, FromValue<BowlingType>(value).Name)
+        {
+        }
+
+        private BowlingType(int value, string name)
+            : base(value, name)
+        {
+        }
+    }
+}
