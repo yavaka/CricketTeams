81d1979e0b18b5e6807fcd7732124a66ec9c1af5
yavaka <yavakasolutions@gmail.com>
2021-03-01 09:52:10 +0000

    Added Batting style enum

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Players/BattingStyle.cs
@@ -0,0 +1,20 @@
+﻿namespace CricketTeams.Domain.Models
+{
+    using CricketTeams.Domain.Common;
+    
+    public class BattingStyle : Enumeration
+    {
+        public static readonly BattingStyle LeftHand = new BattingStyle(1, "Left hand");
+        public static readonly BattingStyle RightHand = new BattingStyle(2, "Right hand");
+
+        private BattingStyle(int value)
+            : this(value, FromValue<BattingStyle>(value).Name)
+        {
+        }
+
+        private BattingStyle(int value, string name)
+            : base(value, name)
+        {
+        }
+    }
+}
