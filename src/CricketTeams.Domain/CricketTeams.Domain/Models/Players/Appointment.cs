2e94d45290264224c0e9e5c3a6a4c612350eb234
yavaka <yavakasolutions@gmail.com>
2021-03-09 09:44:44 +0000

    Added appointment value object.

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Players/Appointment.cs
@@ -0,0 +1,61 @@
+﻿namespace CricketTeams.Domain.Models
+{
+    using System;
+    using CricketTeams.Domain.Common;
+    using CricketTeams.Domain.Exceptions;
+
+    using static ModelConstants.Common;
+
+    public class Appointment : ValueObject
+    {
+        internal Appointment(
+            string description,
+            string service,
+            string appointmentWith,
+            DateTime startDate,
+            DateTime endDate) 
+        {
+            Validate(description, service, appointmentWith);
+
+            this.Description = description;
+            this.Service = service;
+            this.AppointmentWith = appointmentWith;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+
+        private Appointment(
+            string description,
+            string service,
+            string appointmentWith)
+        {
+            this.Description = description;
+            this.Service = service;
+            this.AppointmentWith = appointmentWith;
+
+            this.StartDate = default!;
+            this.EndDate = default!;
+        }
+
+        public string Description { get;private set; }
+        public string Service { get; private set; }
+        public string AppointmentWith { get; private set; }
+        public DateTime? StartDate { get; private set; }
+        public DateTime? EndDate { get; private set; }
+        public TimeSpan? Duration => StartDate - EndDate;
+
+        private void Validate(string description, string service, string appointmentWith)
+        {
+            ValidateDescription(description);
+            //ValidateService(service);
+            //ValidateAppointmentWith(appointmentWith);
+        }
+
+        private void ValidateDescription(string description)
+            => Guard.ForStringLength<InvalidAppointmentException>(
+                description,
+                MinDescriptionLength,
+                MaxDescriptionLength,
+                nameof(this.Description));
+    }
+}
