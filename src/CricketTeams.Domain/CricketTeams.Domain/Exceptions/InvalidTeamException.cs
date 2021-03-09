257f1524ea8eecac8ebdb2140d4fd19900701f77
yavaka <yavakasolutions@gmail.com>
2021-03-09 11:48:54 +0000

    Added invalid team exception

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Exceptions/InvalidTeamException.cs
@@ -0,0 +1,11 @@
+﻿namespace CricketTeams.Domain.Exceptions
+{
+    public class InvalidTeamException : BaseDomainException
+    {
+        public InvalidTeamException()
+        {
+        }
+
+        public InvalidTeamException(string error) => this.Error = error;
+    }
+}
