7d3115bfb1d200035145106d6be2e073263e7388
yavaka <yavakasolutions@gmail.com>
2021-03-12 09:57:30 +0000

    Added invalid match exception.

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Exceptions/InvalidMatchException.cs
@@ -0,0 +1,11 @@
+﻿namespace CricketTeams.Domain.Exceptions
+{
+    public class InvalidMatchException : BaseDomainException
+    {
+        public InvalidMatchException()
+        {
+        }
+
+        public InvalidMatchException(string error) => this.Error = error;
+    }
+}
