bc15883614a9ae5114ce4335515040dd78cb5b2b
yavaka <yavakasolutions@gmail.com>
2021-03-15 11:41:49 +0000

    Added score value object.

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Matches/Score.Inning.cs
@@ -0,0 +1,7 @@
+﻿namespace CricketTeams.Domain.Models.Matches
+{
+    public class ScoreInning
+    {
+        public bool IsCompleted { get; private set; }
+    }
+}
--- src/CricketTeams.Domain/CricketTeams.Domain/Models/Matches/Score.cs
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Matches/Score.cs
@@ -1,6 +1,66 @@
 ﻿namespace CricketTeams.Domain.Models.Matches
 {
-    public class Score
+    using CricketTeams.Domain.Common;
+    using CricketTeams.Domain.Exceptions;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Score : ValueObject
     {
+        internal Score(
+            int oversPerInning,
+            int numberOfInnings,
+            ScoreInning currentInning)
+        {
+            this.OversPerInning = oversPerInning;
+            this.NumberOfInnings = numberOfInnings;
+            this.CurrentInning = currentInning;
+            this.Innings = new ScoreInning[this.NumberOfInnings];
+        }
+
+        public int OversPerInning { get; private set; }
+        public int NumberOfInnings { get; set; }
+        public bool IsMatchEnd { get; private set; } = false;
+        public ScoreInning CurrentInning { get; private set; }
+        public ICollection<ScoreInning> Innings { get; private set; }
+
+        public Score EndMatch()
+        {
+            ValidateIsMatchEnd();
+
+            this.IsMatchEnd = true;
+
+            return this;
+        }
+
+        public Score EndCurrentInning()
+        {
+            if (this.CurrentInning.IsCompleted)
+            {
+                ValidateInning();
+
+                this.Innings.Add(
+                    this.CurrentInning != null ?
+                    this.CurrentInning :
+                    throw new InvalidScoreException("Invalid inning!"));
+            }
+            return this;
+        }
+
+        private void ValidateInning()
+        {
+            if (this.Innings.Count > this.NumberOfInnings)
+            {
+                throw new InvalidScoreException($"Maximum number of innings can be {this.NumberOfInnings}");
+            }
+        }
+
+        private void ValidateIsMatchEnd()
+        {
+            if (this.Innings.All(i => i == default))
+            {
+                throw new InvalidScoreException($"Match is still in progress.");
+            }
+        }
     }
 }
