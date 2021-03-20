0f54877d7ea663b37fb555017fd32c8a495ed4ff
yavaka <yavakasolutions@gmail.com>
2021-03-20 15:06:56 +0000

    Added player out types enumeration.

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Matches/PlayerOutTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace CricketTeams.Domain.Models.Matches
+{
+    using CricketTeams.Domain.Common;
+    
+    public class PlayerOutTypes : Enumeration
+    {
+        public static readonly PlayerOutTypes Wicket = new PlayerOutTypes(1, "Wicket");
+        public static readonly PlayerOutTypes Catch = new PlayerOutTypes(2, "Catch");
+        public static readonly PlayerOutTypes RunOut = new PlayerOutTypes(3, "Run out");
+        public static readonly PlayerOutTypes LegBeforeWicket = new PlayerOutTypes(4, "Leg before wicket");
+        public static readonly PlayerOutTypes HitBallTwice = new PlayerOutTypes(5, "Hit ball twice");
+
+        private PlayerOutTypes(int value)
+            : this(value, FromValue<BowlingType>(value).Name)
+        {
+        }
+
+        private PlayerOutTypes(int value, string name)
+            : base(value, name)
+        {
+        }
+    }
+}
