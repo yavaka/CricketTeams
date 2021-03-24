994430c96d2d8c6eaf5f78e1e09dd2ff564a1dd9
yavaka <yavakasolutions@gmail.com>
2021-03-24 18:58:26 +0000

    Added invalid inning exception

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Exceptions/InvalidInningException.cs
@@ -0,0 +1,11 @@
+﻿namespace CricketTeams.Domain.Exceptions
+{
+    public class InvalidInningException : BaseDomainException
+    {
+        public InvalidInningException()
+        {
+        }
+
+        public InvalidInningException(string error) => this.Error = error;
+    }
+}
