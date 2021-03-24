921b768149b6b2f331d6244ae4b5639c51eb8e3b
yavaka <yavakasolutions@gmail.com>
2021-03-24 09:34:29 +0000

    Added match bowler

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Players/MatchStat/Match.Bowler.cs
@@ -0,0 +1,30 @@
+﻿namespace CricketTeams.Domain.Models.Players.MatchStat
+{
+    using CricketTeams.Domain.Common;
+    
+    public class MatchBowler : ValueObject
+    {
+        internal MatchBowler(int wideBalls, int wickets)
+        {
+            this.WideBalls = wideBalls;
+            this.Wickets = wickets;
+        }
+
+        public int WideBalls { get; private set; }
+        public int Wickets { get; private set; }
+
+        public MatchBowler IncreaseWideBalls() 
+        {
+            this.WideBalls++;
+
+            return this;
+        }
+
+        public MatchBowler IncreaseWickets()
+        {
+            this.Wickets++;
+
+            return this;
+        }
+    }
+}
