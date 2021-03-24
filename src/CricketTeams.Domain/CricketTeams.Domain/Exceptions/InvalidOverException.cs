1e348f38d728bf351bb438cb4b164fd10f78b1aa
yavaka <yavakasolutions@gmail.com>
2021-03-24 12:45:02 +0000

    Added Invalid Over Exception

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Exceptions/InvalidOverException.cs
@@ -0,0 +1,11 @@
+﻿namespace CricketTeams.Domain.Exceptions
+{
+    public class InvalidOverException : BaseDomainException
+    {
+        public InvalidOverException()
+        {
+        }
+
+        public InvalidOverException(string error) => this.Error = error;
+    }
+}
