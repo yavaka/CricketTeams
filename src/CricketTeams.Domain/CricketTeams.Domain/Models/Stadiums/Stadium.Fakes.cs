eebbc99fd7e473bc419680b1af76469f9aa19826
yavaka <yavakasolutions@gmail.com>
2021-03-29 15:35:09 +0100

    Added team players and stadium fake factories.

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Stadiums/Stadium.Fakes.cs
@@ -0,0 +1,31 @@
+﻿namespace CricketTeams.Domain.Models.Stadiums
+{
+    using Bogus;
+    using CricketTeams.Domain.Common;
+    using FakeItEasy;
+    using System;
+
+    public class StadiumFakes : IDummyFactory
+    {
+        public Priority Priority => Priority.Default;
+
+        public bool CanCreate(Type type) => type == typeof(Stadium);
+
+        public object? Create(Type type) => Data.GetStadium();
+
+        public static class Data
+        {
+            public static Stadium GetStadium(int id = 1)
+                => new Faker<Stadium>()
+                    .CustomInstantiator(f => new Stadium(
+                        f.Name.FindName(),
+                        f.Address.FullAddress(),
+                        f.Random.Number(100, 100000),
+                        f.Internet.Url(),
+                        f.Company.CompanyName()))
+                    .Generate()
+                    .SetId(id);
+        }
+
+    }
+}
---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Teams/Players.Fakes.cs
@@ -0,0 +1,49 @@
+﻿namespace CricketTeams.Domain.Models.Teams
+{
+    using Bogus;
+    using CricketTeams.Domain.Models.Players;
+    using FakeItEasy;
+    using System;
+    using System.Linq;
+
+    public class PlayersFakes : IDummyFactory
+    {
+        public Priority Priority => Priority.Default;
+
+        public bool CanCreate(Type type) => type == typeof(Players);
+
+        public object? Create(Type type) => Data.GetPlayers();
+
+        public static class Data
+        {
+            public static Players GetPlayers()
+            {
+                var captain = A.Dummy<Player>();
+                var wicketKeeper = A.Dummy<Player>();
+                var twelftman = A.Dummy<Player>();
+                var restOfPlayers = PlayerFakes.Data.GetPlayers();
+
+                var players = new Faker<Players>()
+                    .CustomInstantiator(f => new Players(
+                        captain,
+                        wicketKeeper,
+                        twelftman)).Generate();
+
+                players.AddAllRounder(captain);
+                players.AddBatsman(wicketKeeper);
+
+                foreach (var batsman in restOfPlayers)
+                {
+                    players.AddBatsman(batsman);
+                }
+
+                foreach (var bowler in restOfPlayers.Take(5))
+                {
+                    players.AddBowler(bowler);
+                }
+
+                return players;
+            }
+        }
+    }
+}
