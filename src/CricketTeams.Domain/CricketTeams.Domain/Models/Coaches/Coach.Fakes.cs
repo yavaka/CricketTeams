c44b078070251c7481ff9def61aa4af63fe511da
yavaka <yavakasolutions@gmail.com>
2021-03-31 09:22:08 +0100

    Added coach fake factory

---
+++ src/CricketTeams.Domain/CricketTeams.Domain/Models/Coaches/Coach.Fakes.cs
@@ -0,0 +1,34 @@
+﻿namespace CricketTeams.Domain.Models.Coaches
+{
+    using Bogus;
+    using CricketTeams.Domain.Common;
+    using CricketTeams.Domain.Models.Players;
+    using FakeItEasy;
+    using System;
+
+    public class CoachFakes : IDummyFactory
+    {
+        public Priority Priority => Priority.Default;
+
+        public bool CanCreate(Type type) => type == typeof(Coach);
+
+        public object? Create(Type type) => Data.GetCoach();
+
+        public static class Data
+        {
+            public static Coach GetCoach(int id = 1)
+                => new Faker<Coach>()
+                    .CustomInstantiator(f => new Coach(
+                        f.Person.FirstName,
+                        f.Person.LastName,
+                        f.Person.UserName,
+                        f.Random.Number(10, 80),
+                        f.Address.Country(),
+                        f.Image.PlaceImgUrl(),
+                        BattingStyle.RightHand,
+                        A.Dummy<BowlingStyle>()))
+                    .Generate()
+                    .SetId(id);
+        }
+    }
+}
