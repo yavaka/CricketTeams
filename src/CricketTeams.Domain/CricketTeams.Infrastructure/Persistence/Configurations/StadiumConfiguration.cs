3ecf2445a7ef2d49a27f6e77d1e0fa06ab4fd43b
yavaka <yavakasolutions@gmail.com>
2021-06-04 12:24:42 +0100

    Added Stadium configuration

---
+++ src/CricketTeams.Domain/CricketTeams.Infrastructure/Persistence/Configurations/StadiumConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace CricketTeams.Infrastructure.Persistence.Configurations
+{
+    using CricketTeams.Domain.Models.Stadiums;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using static CricketTeams.Domain.Models.ModelConstants.Common;
+    using static CricketTeams.Domain.Models.ModelConstants.Stadium;
+
+    internal class StadiumConfiguration : IEntityTypeConfiguration<Stadium>
+    {
+        public void Configure(EntityTypeBuilder<Stadium> builder)
+        {
+            // Id
+            builder.HasKey(i => i.Id);
+
+            // Name
+            builder.Property(n => n.Name)
+                .HasMaxLength(MaxNameLength)
+                .IsRequired();
+
+            // Capacity
+            builder.Property(c => c.Capacity)
+                .HasMaxLength(MaxCapacityLength)
+                .IsRequired();
+
+            // Website url
+            builder.Property(wUrl => wUrl.WebsiteUrl)
+                .HasMaxLength(MaxUrlLength);
+        }
+    }
+}
