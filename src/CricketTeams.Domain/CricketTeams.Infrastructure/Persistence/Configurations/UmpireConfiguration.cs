39cf80ef89295305de437d4e5c565a8196bf4037
yavaka <yavakasolutions@gmail.com>
2021-06-04 12:53:51 +0100

    Added Umpire configuration

---
+++ src/CricketTeams.Domain/CricketTeams.Infrastructure/Persistence/Configurations/UmpireConfiguration.cs
@@ -0,0 +1,34 @@
+﻿namespace CricketTeams.Infrastructure.Persistence.Configurations
+{
+    using CricketTeams.Domain.Models.Matches;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using static CricketTeams.Domain.Models.ModelConstants.Common;
+
+    internal class UmpireConfiguration : IEntityTypeConfiguration<Umpire>
+    {
+        public void Configure(EntityTypeBuilder<Umpire> builder)
+        {
+            // Id
+            builder.HasKey("Id");
+
+            // First name
+            builder
+                .Property(u => u.FirstName)
+                .HasMaxLength(MaxNameLength)
+                .IsRequired();
+
+            // Last name
+            builder
+                .Property(u => u.LastName)
+                .HasMaxLength(MaxNameLength)
+                .IsRequired();
+
+            // Age
+            builder
+                .Property(u => u.Age)
+                .HasMaxLength(MaxAge)
+                .IsRequired();
+        }
+    }
+}
