d149db748514f927be6146b98c115ee8c63e06e8
yavaka <yavakasolutions@gmail.com>
2021-06-10 10:25:47 +0100

    Added Over configuration

---
+++ src/CricketTeams.Domain/CricketTeams.Infrastructure/Persistence/Configurations/OverConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace CricketTeams.Infrastructure.Persistence.Configurations
+{
+    using CricketTeams.Domain.Models.Scores;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class OverConfiguration : IEntityTypeConfiguration<Over>
+    {
+        public void Configure(EntityTypeBuilder<Over> builder)
+        {
+            builder.HasKey("Id");
+
+            // Bowler
+            builder
+                .Property(o => o.Bowler)
+                .IsRequired();
+
+            // Striker
+            builder
+                .Property(o => o.Striker)
+                .IsRequired();
+
+            // Non striker
+            builder
+                .Property(o => o.NonStriker)
+                .IsRequired();
+
+            // Balls
+            builder
+                .OwnsMany(o => o.Balls, b => 
+                {
+                    b.WithOwner().HasForeignKey("OwnerId");
+                });
+        }
+    }
+}
