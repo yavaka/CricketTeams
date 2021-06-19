8513a978c8d7c8ee9cdd61bcbc910867529f93c6
yavaka <yavakasolutions@gmail.com>
2021-06-19 15:40:56 +0100

    Added IRepository interface

---
+++ src/CricketTeams.Application/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace CricketTeams.Application.Contracts
+{
+    using CricketTeams.Domain.Common;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IRepository<out TEntity>
+        where TEntity : IAggregateRoot
+    {
+        IQueryable<TEntity> All();
+
+        Task<int> SaveChanges(CancellationToken cancellationToken = default);
+    }
+}
