8e8601f4759b216c47cec5276ff7e348bc36a719
yavaka <yavakasolutions@gmail.com>
2021-06-19 16:51:19 +0100

    Added Data repository

---
+++ src/CricketTeams.Infrastructure/Persistence/Repositories/DataRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace CricketTeams.Infrastructure.Persistence.Repositories
+{
+    using CricketTeams.Application.Contracts;
+    using CricketTeams.Domain.Common;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class DataRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IAggregateRoot
+    {
+        private readonly CricketTeamsDbContext _data;
+
+        public DataRepository(CricketTeamsDbContext data) => this._data = data;
+
+        public IQueryable<TEntity> All() => this._data.Set<TEntity>();
+
+        public Task<int> SaveChanges(CancellationToken cancellationToken = default)
+            => this._data.SaveChangesAsync(cancellationToken);
+    }
+}
